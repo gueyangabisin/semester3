956acacb24240d3be2953396e343f6be0b54c1c8
Muhammad Rizal Indriawan <147900972+gueyangabisin@users.noreply.github.com>
2025-01-15 11:06:56 +0700

    Add files via upload
    pewarisan

---
+++ inheritance/Enemy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author deekte
+ */
+public class Enemy {
+    String name = "rika";
+     int Hp = 23;
+     int attackPoin = 85;
+     
+     void attack(){
+         System.out.println("Serang!");
+     }
+    
+}
---
+++ inheritance/Pocong.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author deekte
+ */
+class Pocong extends Enemy {
+    void jump(){
+        System.out.println("Pocong Loncat-Loncat");
+    }
+    void attack(){
+        System.out.println("Serang Pocong !!!!!");
+    }
+    
+}
